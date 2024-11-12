54a1e20421259a40be23a3d1557ccc90e19ddd49
NYC0326 <namyc326@gmail.com>
2024-11-12 11:23:54 +0900

    ChatBot Minor Updates

--- src/components/SmartSearch.tsx
+++ src/components/SmartSearch.tsx
@@ -5,6 +5,7 @@ import 'react-chatbot-kit/build/main.css';
 import ActionProvider from '../bot/ActionProvider';
 import MessageParser from '../bot/MessageParser';
 import config from '../bot/config';
+import '../styles/SmartSearch.css';
 
 const SmartSearch: React.FC = () => {
   return (
@@ -22,22 +23,12 @@ const SmartSearch: React.FC = () => {
         <h1 style={{ margin: 0 }}>EasyPICK</h1>
         <p style={{ margin: 0 }}>챗봇과 대화하세요!</p>
       </div>
-      <div
-        style={{
-          flex: 1,
-          padding: '1rem',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <div>
-          <Chatbot
-            config={config}
-            actionProvider={ActionProvider}
-            messageParser={MessageParser}
-          />
-        </div>
+      <div>
+        <Chatbot
+          config={config}
+          actionProvider={ActionProvider}
+          messageParser={MessageParser}
+        />
       </div>
       <div
         style={{

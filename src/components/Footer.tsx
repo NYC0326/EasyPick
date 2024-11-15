fd68072f447262b234dd893d0915b0fd91f6609a
tt-adisoh <ktx030430@naver.com>
2024-11-15 17:23:19 +0900

    관심상품 템플릿, 하단바 svg 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ReactComponent as Home } from '../icon/house-regular.svg';
-import { ReactComponent as Heart } from '../icon/heart-regular.svg';
-import { ReactComponent as Tag } from '../icon/tag-regular.svg';
-import { ReactComponent as SmartSearch } from '../icon/chatbot-regular.svg';
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { ReactComponent as HomeRegular } from '../icon/house-regular.svg';
+import { ReactComponent as HomeSolid } from '../icon/house-solid.svg';
+import { ReactComponent as HeartRegular } from '../icon/heart-regular.svg';
+import { ReactComponent as HeartSolid } from '../icon/heart-solid.svg';
+import { ReactComponent as TagRegular } from '../icon/tag-regular.svg';
+import { ReactComponent as TagSolid } from '../icon/tag-solid.svg';
+import { ReactComponent as SmartSearchRegular } from '../icon/chatbot-regular.svg';
+import { ReactComponent as SmartSearchSolid } from '../icon/chatbot-solid.svg';
 
-const Footer: React.FC = (
-  currentPage
-) => {
-  if (currentPage == "Home") {
-  }
+const Footer: React.FC = () => {
+  const [HomeIcon, setHomeIcon] = useState(HomeRegular);
+  const [HeartIcon, setHeartIcon] = useState(HeartRegular);
+  const [TagIcon, setTagIcon] = useState(TagRegular);
+  const [SmartSearchIcon, setSmartSearchIcon] = useState(SmartSearchRegular);
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    setHomeIcon(location.pathname === "/" ? HomeSolid : HomeRegular);
+    setHeartIcon(location.pathname === "/favorite" ? HeartSolid : HeartRegular);
+    setTagIcon(location.pathname === "/today-special" ? TagSolid : TagRegular);
+    setSmartSearchIcon(location.pathname === "/smart-search" ? SmartSearchSolid : SmartSearchRegular);
+  }, [location.pathname]);  // 경로가 변경될 때마다 useEffect 실행
 
   const handleSmartSearchClick = () => {
     navigate('/smart-search'); // 스마트 검색 화면으로 이동
@@ -54,7 +67,7 @@ const Footer: React.FC = (
         }}
         onClick={handleFavoriteClick}
       >
-        <Heart width="40" height="40" />
+        <HeartIcon width="40" height="40" />
         <p style={BottomStyle}>관심상품</p>
       </div>
       <div
@@ -67,7 +80,7 @@ const Footer: React.FC = (
         }}
         onClick={handleSmartSearchClick}
       >
-        <SmartSearch width="40" height="40" />
+        <SmartSearchIcon width="40" height="40" />
         <p style={BottomStyle}>스마트 검색</p>
       </div>
       <div
@@ -80,7 +93,7 @@ const Footer: React.FC = (
         }}
         onClick={handleHomeClick}
       >
-      <Home width="40" height="40"/>
+      <HomeIcon width="40" height="40"/>
         <p style={BottomStyle}>메인 화면</p>
       </div>
       <div
@@ -93,7 +106,7 @@ const Footer: React.FC = (
         }}
         onClick={handleTodaySpecialClick}
       >
-        <Tag width="40" height="40" />
+        <TagIcon width="40" height="40" />
         <p style={BottomStyle}>오늘의 특가</p>
       </div>
     </footer>

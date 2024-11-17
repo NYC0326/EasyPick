6622d0cc74335e73d56be0e0ddbb5d7e0917f7d8
tt-adisoh <ktx030430@naver.com>
2024-11-17 19:52:42 +0900

    prettier

--- src/components/Favorite.tsx
+++ src/components/Favorite.tsx
@@ -7,23 +7,26 @@ import ItemTemplate from './ItemTemplate';
 const length = 2;
 
 const Favorite: React.FC = () => (
-<div>
-  <Header /> { }
-  <hr color="gray"></hr>
-    <a>전체 상품</a> 
-    <a style={{color: '#1F64BF'}}> 총 {length}개 </a>
-  <hr color="gray"></hr>
-  
-  {Array.from({ length }).map((_, index) => (
-  <ItemTemplate
-    name="안원당 갈비탕"
-    weight="700g"
-    imageUrl="https://img.danawa.com/prod_img/500000/876/270/img/68270876_1.jpg?shrink=130:130&_v=20241005073111"
-    purchaseLink="https://prod.danawa.com/bridge/loadingBridge.html?cate1=46803&cate2=56857&cate3=56877&cate4=0&pcode=68270876&cmpnyc=ED903&safe_trade=4&fee_type=T&link_pcode=2749746889&package=0&setpc=0"
-  />
-  ))}
-  <Footer /> {/* Footer를 페이지 하단에 추가 */}
-</div>
+  <div>
+    <Header />
+    <hr color="gray" />
+    <div style={{ padding: '0 20px' }}>
+      <a>전체 상품</a>
+      <a style={{ color: '#1F64BF' }}> 총 {length}개 </a>
+    </div>
+    <hr color="gray" />
+
+    {Array.from({ length }).map((_, index) => (
+      <ItemTemplate
+        key={index}
+        name="안원당 갈비탕"
+        weight="700g"
+        imageUrl="https://img.danawa.com/prod_img/500000/876/270/img/68270876_1.jpg?shrink=130:130&_v=20241005073111"
+        purchaseLink="https://prod.danawa.com/bridge/loadingBridge.html?cate1=46803&cate2=56857&cate3=56877&cate4=0&pcode=68270876&cmpnyc=ED903&safe_trade=4&fee_type=T&link_pcode=2749746889&package=0&setpc=0"
+      />
+    ))}
+    <Footer />
+  </div>
 );
 
 export default Favorite;
--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -8,31 +8,40 @@ import SearchHeader from './SearchHeader';
 const length = 3;
 const searchIcon = require('../icon/search-icon.png');
 
-const SearchResult: React.FC = () => (
-  <div>
-    <Header /> 
-    <SearchHeader />
-    <div style={{display: 'flex'}}>
-      <a style={{width: '15%'}}>전체 상품</a> 
-      <a style={{color: '#1F64BF', width: '60%'}}> 총 {length}개 </a>
-      <select>
-        <option value="popular">인기순</option>
-        <option value="recent">최신순</option>
-        <option value="lowprice">낮은 가격순</option>
-      </select>
+const SearchResult: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <SearchHeader />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: '0 20px',
+        }}
+      >
+        <a style={{ width: '15%' }}>전체 상품</a>
+        <a style={{ color: '#1F64BF', width: '60%' }}> 총 {length}개 </a>
+        <select>
+          <option value="popular">인기순</option>
+          <option value="recent">최신순</option>
+          <option value="lowprice">낮은 가격순</option>
+        </select>
+      </div>
+      <hr color="gray" />
+
+      {Array.from({ length }).map((_, index) => (
+        <ItemTemplate
+          key={index}
+          name="안원당 갈비탕"
+          weight="700g"
+          imageUrl="https://img.danawa.com/prod_img/500000/876/270/img/68270876_1.jpg?shrink=130:130&_v=20241005073111"
+          purchaseLink="https://prod.danawa.com/bridge/loadingBridge.html?cate1=46803&cate2=56857&cate3=56877&cate4=0&pcode=68270876&cmpnyc=ED903&safe_trade=4&fee_type=T&link_pcode=2749746889&package=0&setpc=0"
+        />
+      ))}
+      <Footer />
     </div>
-    <hr color="gray"></hr>
-    
-    {Array.from({ length }).map((_, index) => (
-      <ItemTemplate
-        name="안원당 갈비탕"
-        weight="700g"
-        imageUrl="https://img.danawa.com/prod_img/500000/876/270/img/68270876_1.jpg?shrink=130:130&_v=20241005073111"
-        purchaseLink="https://prod.danawa.com/bridge/loadingBridge.html?cate1=46803&cate2=56857&cate3=56877&cate4=0&pcode=68270876&cmpnyc=ED903&safe_trade=4&fee_type=T&link_pcode=2749746889&package=0&setpc=0"
-      />
-    ))}
-    <Footer /> {/* Footer를 페이지 하단에 추가 */}
-  </div>
-);
+  );
+};
 
 export default SearchResult;

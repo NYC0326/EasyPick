1204baca6d2e22023bc10dfaf39caedad4b6d143
NYC0326 <namyc326@gmail.com>
2024-11-19 09:53:09 +0900

    Updates

--- src/components/ItemTemplate.tsx
+++ src/components/ItemTemplate.tsx
@@ -6,16 +6,16 @@ interface ItemProp {
   weight: string;
   imageUrl: string;
   purchaseLink: string;
+  discount?: string;
 }
 
 const cardStyle = {
   border: '1px solid #e5e7eb',
   borderRadius: '1rem',
-  padding: '1.5rem',
+  padding: '0.5rem',
   textAlign: 'center' as const,
   cursor: 'pointer',
   position: 'relative' as const,
-  display: 'flex',
 };
 
 const ItemTemplate: React.FC<ItemProp> = ({
@@ -23,6 +23,7 @@ const ItemTemplate: React.FC<ItemProp> = ({
   weight,
   imageUrl,
   purchaseLink,
+  discount,
 }) => {
   const [isFavorite, setIsFavorite] = useState(true);
   const toggleFavorite = () => {
@@ -30,73 +31,80 @@ const ItemTemplate: React.FC<ItemProp> = ({
   };
   return (
     <div style={cardStyle}>
-      <button
-        onClick={toggleFavorite}
-        style={{
-          position: 'absolute',
-          top: '1rem',
-          right: '0.5rem',
-          backgroundColor: 'transparent',
-          border: 'none',
-          cursor: 'pointer',
-        }}
-      >
-        <img
-          src={
-            !isFavorite
-              ? require('../icon/no_click_heart.png')
-              : require('../icon/clicked_heart.png')
-          }
-          alt={!isFavorite ? '찜하지 않은 상태' : '찜한 상태'}
-          style={{ width: '24px', height: '24px' }}
-        />
-      </button>
-
-      <div
-        style={{ display: 'inline', width: '25%', textAlign: 'center' }}
-        onClick={() => window.open(purchaseLink, '_blank')}
-      >
-        <img
-          src={imageUrl}
-          alt={name}
+      <div style={{ display: 'flex' }}>
+        <button
+          onClick={toggleFavorite}
           style={{
-            width: '100px',
-            height: '100px',
-            borderRadius: '0.5rem',
-            objectFit: 'cover',
+            position: 'absolute',
+            top: '1rem',
+            right: '0.5rem',
+            backgroundColor: 'transparent',
+            border: 'none',
+            cursor: 'pointer',
           }}
-        />
-        <a style={{ color: '#F8B91F', textAlign: 'center' }}> ★★★★★ </a>
-      </div>
+        >
+          <img
+            src={
+              !isFavorite
+                ? require('../icon/no_click_heart.png')
+                : require('../icon/clicked_heart.png')
+            }
+            alt={!isFavorite ? '찜하지 않은 상태' : '찜한 상태'}
+            style={{ width: '24px', height: '24px' }}
+          />
+        </button>
 
-      <div style={{ width: '75%' }}>
-        <h3
-          style={{
-            margin: '0.5rem 0',
-            fontSize: '1.2rem',
-            color: '#1f2937',
-            textAlign: 'left',
-          }}
+        <div
+          style={{ display: 'inline', width: '25%', textAlign: 'center' }}
           onClick={() => window.open(purchaseLink, '_blank')}
         >
-          {name} {weight}
-        </h3>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <p style={{ color: '#707070', width: '10%', textAlign: 'left' }}>
-            1개
-          </p>
-          <p style={{ width: '60%', textAlign: 'left' }}> 100g당 775원 </p>
-          <p
+          <img
+            src={imageUrl}
+            alt={name}
             style={{
-              color: '#1F64BF',
-              width: '30%',
-              fontSize: '1.2rem',
-              textAlign: 'right',
+              width: '100px',
+              height: '100px',
+              borderRadius: '0.5rem',
+              objectFit: 'cover',
             }}
-          >
-            <b>8,140원</b>
-          </p>
+          />
+          <a style={{ color: '#F8B91F', textAlign: 'center' }}> ★★★★★ </a>
         </div>
+
+        <div style={{ width: '75%' }}>
+          <h3
+            style={{
+              margin: '0.5rem 0',
+              fontSize: '1.2rem',
+              color: '#1f2937',
+              textAlign: 'left',
+            }}
+            onClick={() => window.open(purchaseLink, '_blank')}
+          >
+            {name} {weight}
+          </h3>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p style={{ color: '#707070', width: '10%', textAlign: 'left' }}>
+              1개
+            </p>
+            <p style={{ width: '60%', textAlign: 'left' }}> 100g당 775원 </p>
+            <p
+              style={{
+                color: '#1F64BF',
+                width: '30%',
+                fontSize: '1.2rem',
+                textAlign: 'right',
+              }}
+            >
+              <b>8,140원</b>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        {discount && (
+          <p style={{ color: '#F27A5E', textAlign: 'center' }}>{discount}</p>
+        )}
       </div>
     </div>
   );
